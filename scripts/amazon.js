bef6d0797e3e6561772ff5700d8593b2569d01bf
Sarit 'TAWAN' Rungjapoh <107798657+saritrungj@users.noreply.github.com>
2024-02-14 11:11:27 +0700

    Make add too cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,55 +1,27 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090 // 1 dollar = 100 cents
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
-
 let productsHTML = '';
 
-products.forEach((products) => {
+products.forEach((product) => {
   productsHTML += `
       <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${products.image}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${products.name}
+            ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${products.rating.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${products.rating.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${products.priceCents / 100}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -74,7 +46,9 @@ products.forEach((products) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button 
+          button-primary js-add-to-cart" 
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -85,3 +59,27 @@ console.log(productsHTML);
 
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+      console.log(cart);
+    });
+  });
